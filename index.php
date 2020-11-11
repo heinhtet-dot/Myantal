b6d07fdb2a709d13db3f283b47e4e88bccfc3887
Kn@p <60691262+kyaw-nanda-paing@users.noreply.github.com>
2020-11-11 16:35:54 +0630

    first

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title></title>
+
+
+  <!------------CSS-------------->
+  <link rel="stylesheet" href="css/bootstrapmin.css">
+
+
+  <!------------JS-------------->
+  <script src="js/jquerymin.js"></script>
+  <script src="js/poppermin.js"></script>
+  <script src="js/bootstrapmin.js"></script>
+
+
+  <!------------font-------------->
+  <link rel="stylesheet" href="font/fontawesomemin.css">
+
+</head>
+
+
+<body>
+	
+
+
+</body>
+</html>
+
